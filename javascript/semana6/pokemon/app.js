7142438fd3ced2fc83e912e3c0a73835bacd8eca
arnoldbgm <arnold.gallegosm@gmail.com>
2022-10-07 20:49:54 -0500

    Using PokeAPi

--- javascript/semana6/pokemon/app.js
+++ javascript/semana6/pokemon/app.js
@@ -1,6 +1,7 @@
 const btnGetPokemon = document.querySelector("#get-pokemons");
 
 const urlPokemons = "https://pokeapi.co/api/v2/pokemon";
+const container = document.querySelector(".container");
 
 btnGetPokemon.onclick = async function(){
     // Como hacemos para que JS pueda extraer la informacion de
@@ -11,9 +12,53 @@ btnGetPokemon.onclick = async function(){
     // * PAra poder hacer que fetch termie la ejecucicon tenemos
     // que convertir nuestra funcion en async
 
+    // Una promesa es una accion que no se ejecuta
+
     // Debe de haber un async y un await
     // El await espera a que se ejecute la peticion luego 
     // recien la muestrsa
     const respuesta = await fetch(urlPokemons);
-    console.log(respuesta);
+
+    // Crearemos una variable que guarde la informacion q
+    // el servidor responde
+
+    // Lo va a parsear de Json => Objeto
+    // Siempre debemos de convertir un json a un objeto
+    // para que Js lo pueda entedner
+    const data = await respuesta.json();
+    readPokemons(data.results)
 };
+
+
+// ? Esto es by me SI FUNCION 😁😎😎
+// async function showPokemons(){
+//     const respuesta = await fetch(urlPokemons);
+//     const data = await respuesta.json();
+//     data.results.forEach((pokemonElement, index) => {
+//         console.log(pokemonElement);
+//         container.innerHTML +=
+//             `<div class="movie">
+//         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg" alt="" />
+//             <h4>${pokemonElement.name}</h4>
+//             <p>
+//             ${pokemonElement.url}
+//         </p>
+//       </div>`
+//     });
+// }
+
+// showPokemons();
+
+function readPokemons(pokemons){
+    container.innerHTML = "";
+    pokemons.forEach((pokemon,index) =>{
+        container.innerHTML +=
+            `<div class="movie">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg" alt="" />
+            <h4>${pokemon.name}</h4>
+            <p>
+            ${pokemon.url}
+        </p>
+      </div>`
+    });
+}
